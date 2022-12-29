b56702202448f2612835a9796d518122198342fd
ApolloPublicAffairs <118992731+ApolloPublicAffairs@users.noreply.github.com>
2022-12-29 15:55:24 -0500

    Moved form to javascript for embed purposes
    Added two-stage form support and email form.

--- payload.js
+++ payload.js
@@ -1,10 +1,97 @@
+// Initialize global vars
 var storelat
 var storelong
 
+// Initialize the form
+document.addEventListener("DOMContentLoaded",createForm());
+function createForm() {
+    const formContainer = document.getElementById('form-container');
+  
+    formContainer.insertAdjacentHTML("beforeend", `
+    <form id="pform" onsubmit="PayloadRun()" autocomplete="on">
+    <div class="form-group">
+        <label for="firstName">First Name</label>
+        <input type="text" class="form-control" id="firstName" autofill="given-name" autocomplete="given-name" required>
+      </div>
+      <div class="form-group">
+        <label for="lastName">Last Name</label>
+        <input type="text" class="form-control" id="lastName" autofill="family-name" autocomplete="family-name" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" autocomplete="email"  required>
+      </div>
+         <div class="form-group">
+            <label for="phoneNumber">Phone Number</label>
+            <input type="tel" class="form-control" id="phoneNumber" autocomplete="tel"  required>
+        </div>
+        <div class="form-group">
+          <label for="postalCode">Postal Code</label>
+          <input type="text" class="form-control" id="postalCode" autocomplete="postal-code"  required>
+        </div>
+    
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    `);
+  }
+const pform = document.getElementById('pform');
+  pform.addEventListener('submit', async function(e) {
+              e.preventDefault();
+              const payload = new FormData(pform);
+              console.log(payload);
+              await PayloadRun();
+              UpdateForm();
+              getMessage();
+          }
+          
+          )
+
+// Run The Geolocation Process
+
+
 async function PayloadRun() {
     await GetLatLong();
-    doit();
+    await doit();
   }
+
+async function getMessage() {
+    const MessageData = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json"
+      }
+    });
+    const MessageObj = await MessageData.json();
+    console.log(MessageObj);
+    const Messagecontent = document.getElementById("MessageDiv");
+    MessageDiv.value = MessageObj.joke;
+}
+
+function UpdateForm() {
+    
+    const formContainer = document.getElementById('form-container');
+    formContainer.innerHTML = `
+    <p>We've found your elected officials in<strong> ${result.result} </strong></p>
+    <p>While Nathan is setting up the database, why not send him a dad joke?</p>
+    <form method="POST" action="mailto:" id="repform" autocomplete="off">  
+        <div class="form-group">
+            <label for="toField">To:</label>
+            <input type="email" class="form-control" id="toField" name="to" placeholder="nathan.carr@apollopublicaffairs.com">
+        </div>
+        <div class="form-group">
+            <label for="ccField">CC:</label>
+            <input type="email" class="form-control" id="ccField" name="cc" placeholder="operations@apollopublicaffairs.com">
+        </div>
+        <div class="form-group">
+            <label for="bccField">BCC:</label>
+            <input type="email" class="form-control" id="bccField" name="bcc" placeholder="Enter BCC email">
+        </div>
+        <div class="form-group">
+            <label for="messageField">Message:</label>
+            <textarea class="form-control" id="MessageDiv" name="body" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Send Email</button>
+        </form>`
+}
 
 function GetLatLong(postalCode) {
       var postalCode = document.getElementById("postalCode").value;
@@ -89,8 +176,7 @@ function doit() {
   console.log("Longtitude is:", lon)
 
   result = lookup(db, lon, lat)
-  document.getElementById("resultreport").innerHTML = "2015 Rep. Order Electoral District is <strong>" + result.result + "</strong>"
-  document.getElementById("stats").innerHTML = result.stats
+  
   
   }).catch(function (error) {
   console.error("Something went wrong")
